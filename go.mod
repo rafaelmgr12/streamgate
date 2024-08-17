14a8b95bced879f66bde694ac919d2f177169410
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2024-08-17 17:38:08 -0300

    create a simple server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rafaelmgr12/streamgate
 
 go 1.18
+
+require github.com/go-chi/chi/v5 v5.1.0
--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
 
-	fmt.Println("Hello, World")
+	r := chi.NewRouter()
+
+	// Status endpoint
+	r.Get("/status", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("API Gateway is up and running"))
+	})
+
+	// Inicia o servidor HTTP
+	log.Println("API Gateway running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
